79fa465fba460a55edc34fc314c1eae0fa332620
Roland Weiss <rweiss@rawLand.fritz.box>
2017-08-21 15:58:38 +0200

    preparing step 6

--- recreate_os_project_05.sh
+++ recreate_os_project_05.sh
@@ -10,7 +10,4 @@ oc new-app myproject/hello-service
 
 oc expose dc hello-service --port 8080
 
-WEB_UI_HOST=$(oc get route -o=jsonpath='{.spec.host}'  web-ui)
-oc create  route edge --hostname ${WEB_UI_HOST} --path=/hello --service hello-service
-
 oc create  route edge --insecure-policy=Redirect --hostname=$(oc get route -o=jsonpath='{.spec.host}' web-ui) --path=/hello --service hello-service

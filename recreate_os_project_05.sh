8893d53aadc3d81fefa934eeb0e366bd1a7ca52f
Roland Weiss <rweiss@rawland.fritz.box>
2017-09-04 14:46:15 +0200

    fixing bug in step_05
    (cherry picked from commit 61ec449)

--- recreate_os_project_05.sh
+++ recreate_os_project_05.sh
@@ -10,4 +10,5 @@ oc new-app myproject/hello-service
 
 oc expose dc hello-service --port 8080
 
-oc create  route edge --insecure-policy=Redirect --hostname=$(oc get route -o=jsonpath='{.spec.host}' web-ui) --path=/hello --service hello-service
+WEB_UI_HOST=$(oc get route -o=jsonpath='{.spec.host}'  web-ui)
+oc create  route edge --hostname ${WEB_UI_HOST} --path=/hello --service hello-service
